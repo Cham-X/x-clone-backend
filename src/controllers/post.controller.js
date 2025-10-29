1464aa009e08cacbd2a5880048776e3d8b1f3261
Shamsudee Zakariyyah <92693681+Cham-X@users.noreply.github.com>
2025-10-29 14:15:20 +0100

    ...

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -1,10 +1,12 @@
 import asyncHandler from 'express-async-handler'
 import Post from "../models/post.model.js";
 import User from '../models/user.model.js';
+import Comment from '../models/comment.model.js';
+import Notification from '../models/notification.model.js';
 import { getAuth } from "@clerk/express";
-import cloudinary from "../config/cloudinary.js";
-+import Comment from '../models/comment.model.js';
-+import Notification from '../models/notification.model.js';
+import cloudinary from "../config/cloudinary.js"; 
+import Comment from '../models/comment.model.js';
+import Notification from '../models/notification.model.js';
 
 
 
