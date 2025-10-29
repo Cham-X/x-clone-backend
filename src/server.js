3edd4bfc75b1ffbcfc708554ebf6d8d1b94c4c07
Shamsudee Zakariyyah <92693681+Cham-X@users.noreply.github.com>
2025-10-29 11:01:47 +0100

    Implement user profile management and authentication middleware; add user routes and integrate Cloudinary for image handling

--- src/server.js
+++ src/server.js
@@ -1,12 +1,21 @@
 import express from "express";
+import cors from 'cors';
+import { clerkMiddleware } from '@clerk/express';
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
+import userRoutes from "./routes/user.route.js"
 
 const app = express();
+
+app.use(cors())
+app.use(express.json())
+
+app.use(clerkMiddleware())
 
 connectDB();
 
 
 app.get("/", (req, res) => res.send("Hello from server"));
+app.use("/api/users",userRoutes)
 
 app.listen(ENV.PORT, () => console.log("server is running on port:", ENV.PORT));
